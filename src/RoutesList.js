6cc07931599a87ec311f090bc5b98f7fa4e4cdb4
Justin Clark <jclarksummit@gmail.com>
2023-04-25 19:32:21 -0400

    Update routes

---
+++ src/RoutesList.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { Route, Routes, Navigate, Link } from "react-router-dom";
+import Homepage from "./Homepage";
+import CompanyList from "./CompanyList";
+import CompanyDetail from "./CompanyDetail";
+import NotFound from "./NotFound";
+import JobList from "./JobList";
+
+/** RoutesList
+ *
+ * Contains site-wide routes + global 404 (add expl)
+ *
+ * App -> RoutesList
+ *
+ */
+
+function RoutesList() {
+  return (
+    <Routes>
+      <Route path="/" element={<Homepage />}></Route>
+      <Route path="/companies" element={<CompanyList />}></Route>
+      <Route path="/companies/:handle" element={<CompanyDetail />}></Route>
+      <Route path="/jobs" element={<JobList />}></Route>
+      <Route path="*" element={<NotFound />}></Route>
+    </Routes>
+  );
+}
+
+export default RoutesList;
