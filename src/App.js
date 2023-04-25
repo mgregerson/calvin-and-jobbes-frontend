4530538c1ee5a9b3b36ae5c541e6a6c2a5ae2b6b
Justin Clark <jclarksummit@gmail.com>
2023-04-25 19:32:48 -0400

    Update component order

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import Nav from "./Nav";
+import RoutesList from "./RoutesList";
+import { BrowserRouter } from "react-router-dom";
+
+/** App
+ *
+ * App -> Nav
+ * App -> RoutesList
+ */
 
 function App() {
-  return <div className="App"></div>;
+  return (<div className="App">
+    <BrowserRouter>
+      <Nav />
+      <RoutesList />
+    </BrowserRouter>
+  </div>);
 }
 
 export default App;
