b4aaaf231d03bf9c95a9935a3d9f09b7262db6d6
mgregerson <mgregerson4321@gmail.com>
2023-04-25 20:43:29 -0400

    Added css styling to Navbar

--- src/Nav.js
+++ src/Nav.js
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom";
+import "./Nav.css";
 
 /** Nav
  *
@@ -10,16 +11,24 @@ import { NavLink } from "react-router-dom";
 
 function Nav() {
   return (
-    <nav className="Nav navbar navbar-expand-md">
-      <NavLink to="/" end>
-        Jobly
-      </NavLink>
-      <NavLink to="/companies" end>
-        Companies
-      </NavLink>
-      <NavLink to="/jobs" end>
-        Jobs
-      </NavLink>
+    <nav className="Navigation navbar navbar-expand-md">
+      <div className="container-fluid">
+        <NavLink to="/" end>
+          <a className="navbar-brand">Jobly</a>
+        </NavLink>
+        <ul className="navbar-nav ms-auto">
+          <li className="nav-item me-4">
+            <NavLink to="/companies" end>
+              <a className="nav-link">Companies</a>
+            </NavLink>
+          </li>
+          <li className="nav-item me-4">
+            <NavLink to="/jobs" end>
+              <a className="nav-link">Jobs</a>
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
