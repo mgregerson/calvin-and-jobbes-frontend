e429026eac9f18d322e5cdc80daed212d1fd1979
mgregerson <mgregerson4321@gmail.com>
2023-04-26 19:52:34 -0400

    Wrote tests for CompanyCard component

--- src/CompanyCard.test.js
+++ src/CompanyCard.test.js
@@ -1,54 +1,69 @@
-import { render, screen } from '@testing-library/react';
-import CompanyCard from './CompanyCard';
+import { render } from "@testing-library/react";
+import CompanyCard from "./CompanyCard";
 
 const TEST_COMPANY_LOGO = {
-
   handle: "APP",
   name: "Apple",
   description: "Company with computers",
   numEmployees: 400,
   logoUrl: "logos/logo1.png",
-  jobs: [{
-    "id": 91,
-    "title": "Paramedic",
-    "salary": 122000,
-    "equity": "0.047"
-  }]
-
+  jobs: [
+    {
+      id: 91,
+      title: "Paramedic",
+      salary: 122000,
+      equity: "0.047",
+    },
+  ],
 };
 
 const TEST_COMPANY_NO_LOGO = {
-
   handle: "APP",
   name: "Apple",
   description: "Company with computers",
   numEmployees: 400,
   logoUrl: null,
-  jobs: [{
-    "id": 91,
-    "title": "Paramedic",
-    "salary": 122000,
-    "equity": "0.047"
-  }]
-
+  jobs: [
+    {
+      id: 91,
+      title: "Paramedic",
+      salary: 122000,
+      equity: "0.047",
+    },
+  ],
 };
 
-test('renders without crashing', () => {
+test("renders without crashing", function () {
   render(<CompanyCard company={TEST_COMPANY_LOGO} />);
 });
 
-test('matches snapshot', () => {
-  const { companyCard } = render(<CompanyCard company={TEST_COMPANY_LOGO} />);
-  expect(companyCard).toMatchSnapshot();
+test("matches snapshot", function () {
+  const { container } = render(<CompanyCard company={TEST_COMPANY_LOGO} />);
+  expect(container).toMatchSnapshot();
 });
 
-// test('puts info on page', () => {
-//   const { companyCard } = render(<CompanyCard company={TEST_COMPANY_LOGO} />);
+test("puts info on page if there is a logo", () => {
+  const { container } = render(<CompanyCard company={TEST_COMPANY_LOGO} />);
 
-//   expect(screen.getByText("Apple")).toBeInTheDocument();
-//   expect(screen.getByText("Company with computers")).toBeInTheDocument();
-//   expect(screen.getAllByAltText("Apple")).toBeInTheDocument();
-// });
-
+  expect(container.querySelector(".CompanyCard-company").innerHTML).toEqual(
+    "Apple"
+  );
+  expect(container.querySelector(".CompanyCard-description").innerHTML).toEqual(
+    "Company with computers"
+  );
+  expect(container.querySelector(".CompanyCard-img")).toBeInTheDocument();
+});
 
 // Renders if company has no logo
+
+test("puts info on the page if there is no logo", function () {
+  const { container } = render(<CompanyCard company={TEST_COMPANY_NO_LOGO} />);
+
+  expect(container.querySelector(".CompanyCard-company").innerHTML).toEqual(
+    "Apple"
+  );
+  expect(container.querySelector(".CompanyCard-description").innerHTML).toEqual(
+    "Company with computers"
+  );
+  expect(container.querySelector(".CompanyCard-img")).not.toBeInTheDocument();
+});
