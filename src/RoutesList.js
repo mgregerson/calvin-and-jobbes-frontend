63a26761393cba3fa346a2484442c7728e35de7c
Justin Clark <jclarksummit@gmail.com>
2023-04-27 15:31:18 -0400

    Update routes for login, register, auth

--- src/RoutesList.js
+++ src/RoutesList.js
@@ -4,6 +4,9 @@ import CompanyList from "./CompanyList";
 import CompanyDetail from "./CompanyDetail";
 import NotFound from "./NotFound";
 import JobList from "./JobList";
+import LoginForm from "./LoginForm";
+import SignupForm from "./SignupForm";
+import ProfileForm from "./ProfileForm";
 
 /** RoutesList
  *
@@ -17,6 +20,9 @@ function RoutesList() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />}></Route>
+      <Route path="/login" element={<LoginForm />}></Route>
+      <Route path="/signup" element={<SignupForm />}></Route>
+      <Route path="/profile" element={<ProfileForm />}></Route>
       <Route path="/companies" element={<CompanyList />}></Route>
       <Route path="/companies/:handle" element={<CompanyDetail />}></Route>
       <Route path="/jobs" element={<JobList />}></Route>
